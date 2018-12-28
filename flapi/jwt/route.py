becafda9cd3f27ba74f8c35ed1abd797da37fddc
Adam Romano <manoadamro.imp@gmail.com>
2018-12-28 17:09:54 +0000

    added jwt protected

--- flapi/jwt/route.py
+++ flapi/jwt/route.py
@@ -1,2 +1,24 @@
+from typing import Any, Callable, Dict
+import functools
+from . import errors, rules, builder
+
+
 class JwtProtected:
-    pass
+    def __init__(self, *rules: rules.JWTRule):
+        self.rules = rules
+
+    def __call__(self, func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            token: Dict = builder.Builder.current_token()
+            if not token:
+                raise errors.JWTValidationError(
+                    "client did not supply a token in request header"
+                )
+            if not all(rule(token) for rule in self.rules):
+                raise errors.JWTValidationError(
+                    "one or more checks on the supplied jwt failed"
+                )
+            return func(*args, **kwargs)
+
+        return wrapper
